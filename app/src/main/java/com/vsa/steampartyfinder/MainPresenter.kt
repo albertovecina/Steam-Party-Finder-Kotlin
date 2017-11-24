bdd6c63fc2a68dfe348d15bf196388431591e7a2
Alberto Vecina Sánchez <alberto.vecina@grupohasten.com>
2017-11-24 17:50:59 +0100

    *Initial commit

---
+++ app/src/main/java/com/vsa/steampartyfinder/MainPresenter.kt
@@ -0,0 +1,7 @@
+package com.vsa.steampartyfinder
+
+/**
+ * Created by Alberto Vecina Sánchez on 24/11/17.
+ */
+interface MainPresenter {
+}
---
+++ app/src/main/java/com/vsa/steampartyfinder/MainPresenterImpl.kt
@@ -0,0 +1,11 @@
+package com.vsa.steampartyfinder
+
+
+/**
+ * Created by Alberto Vecina Sánchez on 24/11/17.
+ */
+class MainPresenterImpl(view: MainView) : MainPresenter {
+
+    val mView: MainView = view
+
+}
